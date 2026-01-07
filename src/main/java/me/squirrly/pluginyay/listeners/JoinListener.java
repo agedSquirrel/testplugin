852185d0bfa4eb8e90bc609e29bd4908069e0c8d
agedSquirrel <agedsquirrel500@gmail.com>
2026-01-07 20:55:49 +0200

    not the first commit ig

---
+++ src/main/java/me/squirrly/pluginyay/listeners/JoinListener.java
@@ -0,0 +1,13 @@
+package me.squirrly.pluginyay.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class JoinListener implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage("Welcome! Thanks for joining %player%.");
+    }
+}
